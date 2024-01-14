2ca2c0e64a472bb6e38482f6d057401e07aad994
Jerry Gutierrez <eljerrylee@gmail.com>
2024-01-13 19:31:43 -0600

    Add testing page

---
+++ testing.js
@@ -0,0 +1,12 @@
+/* Author: Jerry Lee Gutierrez
+   Date..: 01-13-2024
+*/
+
+var robot = 10;
+
+robot = 23 * 10;
+
+var favoriteFood = prompt("What is your favorite food?");
+
+console.log("I multiply 10 * 23 equals " + robot);
+console.log("Your favorite food is " + favoriteFood + ".")
