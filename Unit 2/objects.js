bffe3063b710c035f851df8845cfe869466134eb
Jerry Gutierrez <eljerrylee@gmail.com>
2024-08-30 10:53:51 -0500

    Modify Portfolio

--- Unit 2/objects.js
+++
@@ -1,8 +0,0 @@
-const jerry = new Name("Jerry", "Gutierrez", 30, "Male");
-const jane = new Name("Jane", "Liverpool", 28, "Female");
-const molly = new Name("Molly", "Gutierrez", 12, "Female");
-
-console.log(jerry.getInfo());
-console.log(jane.getInfo());
-console.log(molly.getInfo());
-
